e5a544d97bb3d2c4ab820bb5609b35d62c44215d
Sergey Svirsky <zversergey@tut.by>
2015-01-06 19:58:57 +0300

    more test cases

--- Untech.Detect/Program.cs
+++ Untech.Detect/Program.cs
@@ -48,32 +48,217 @@ namespace Untech.Detect
 
             processor.PreProcessingStages.Add(new CloneStage());
             
-            
-            //processor.PreProcessingStages.Add(new BGRContrastStage{ LowerPercentage = 0.15, UpperPercentage = 0.25});
-            processor.PreProcessingStages.Add(SplitChannelsStage.BuildValueExtractor());
+			//// Case 1
+			//processor.PreProcessingStages.Add(SplitChannelsStage.BuildValueExtractor());
+			//processor.PreProcessingStages.Add(new CannyStage());
+			//processor.PostProcessingStages.Add(new CloneStage());
+			//processor.PostProcessingStages.Add(new DetectionStage());
+
+
+			//// Case 2
+			//processor.PreProcessingStages.Add(SplitChannelsStage.BuildValueExtractor());
 			//processor.PreProcessingStages.Add(new HistogramContrastStage());
-            //processor.PreProcessingStages.Add(new BlurStage { Param1 = 9, SmoothType = SmoothType.Gaussian });
-            //processor.PreProcessingStages.Add(SplitChannelsStage.BuildSaturationExtractor());
-            //processor.PreProcessingStages.Add(new GrayContrastStage { LowerPercentage = 0.15, UpperPercentage = 0.25 });
-            //processor.PreProcessingStages.Add(PosterizationStage.BuildDefaultLUT(32));
-            //processor.PreProcessingStages.Add(BlurStage.BuildBlur3());
+			//processor.PreProcessingStages.Add(new CannyStage());
+			//processor.PostProcessingStages.Add(new CloneStage());
+			//processor.PostProcessingStages.Add(new DetectionStage());
+
+			//// Case 3
+			//processor.PreProcessingStages.Add(new BGRContrastStage { LowerPercentage = 0.15, UpperPercentage = 0.25 });
+			//processor.PostProcessingStages.Add(new CloneStage());
+
+			//// Case 4
+			//processor.PreProcessingStages.Add(SplitChannelsStage.BuildValueExtractor());
+			//processor.PostProcessingStages.Add(new CloneStage());
+
+			//// Case 5
+			//processor.PreProcessingStages.Add(new BGRContrastStage { LowerPercentage = 0.15, UpperPercentage = 0.25 });
+			//processor.PreProcessingStages.Add(SplitChannelsStage.BuildValueExtractor());
+			//processor.PostProcessingStages.Add(new CloneStage());
+
+			//// Case 6
+			//processor.PreProcessingStages.Add(SplitChannelsStage.BuildValueExtractor());
+			//processor.PreProcessingStages.Add(new HistogramContrastStage());
+			//processor.PostProcessingStages.Add(new CloneStage());
+
+			////// Case 7
+			//processor.PreProcessingStages.Add(SplitChannelsStage.BuildSaturationExtractor());
+			//processor.PostProcessingStages.Add(new CloneStage());
+
+			////// Case 8
+			//processor.PreProcessingStages.Add(new BGRContrastStage{ LowerPercentage = 0.15, UpperPercentage = 0.25});
+			//processor.PreProcessingStages.Add(SplitChannelsStage.BuildSaturationExtractor());
+			//processor.PostProcessingStages.Add(new CloneStage());
+
+			////// Case 9
+			//processor.PreProcessingStages.Add(new BGRContrastStage { LowerPercentage = 0.15, UpperPercentage = 0.25 });
+			//processor.PreProcessingStages.Add(SplitChannelsStage.BuildValueExtractor());
 			//processor.PreProcessingStages.Add(new BlurStage { Param1 = 9, SmoothType = SmoothType.Gaussian });
+			//processor.PostProcessingStages.Add(new CloneStage());
+
+			////// Case 10
+			//processor.PreProcessingStages.Add(SplitChannelsStage.BuildValueExtractor());
+			//processor.PreProcessingStages.Add(new BlurStage { Param1 = 9, SmoothType = SmoothType.Gaussian });
+			//processor.PostProcessingStages.Add(new CloneStage());
+
+			////// Case 11
+			//processor.PreProcessingStages.Add(new BlurStage { Param1 = 9, SmoothType = SmoothType.Gaussian });
+			//processor.PreProcessingStages.Add(SplitChannelsStage.BuildValueExtractor());
+			//processor.PreProcessingStages.Add(new BlurStage { Param1 = 9, SmoothType = SmoothType.Gaussian });
+			//processor.PostProcessingStages.Add(new CloneStage());
+
+			////// Case 12
+			//processor.PreProcessingStages.Add(SplitChannelsStage.BuildValueExtractor());
+			//processor.PreProcessingStages.Add(new BlurStage { Param1 = 9, SmoothType = SmoothType.Gaussian });
+			//processor.PreProcessingStages.Add(PosterizationStage.BuildDefaultLUT(32));
+			//processor.PostProcessingStages.Add(new CloneStage());
+
+			////// Case 13
+			//processor.PreProcessingStages.Add(SplitChannelsStage.BuildValueExtractor());
+			//processor.PreProcessingStages.Add(new BlurStage { Param1 = 9, SmoothType = SmoothType.Gaussian });
+			//processor.PreProcessingStages.Add(PosterizationStage.BuildDefaultLUT(8));
+			//processor.PostProcessingStages.Add(new CloneStage());
+
+
+			////// Case 14
+			//processor.PreProcessingStages.Add(SplitChannelsStage.BuildValueExtractor());
+			//processor.PreProcessingStages.Add(new BlurStage { Param1 = 9, SmoothType = SmoothType.Gaussian });
+			//processor.PreProcessingStages.Add(PosterizationStage.BuildDefaultLUT(6));
+			//processor.PostProcessingStages.Add(new CloneStage());
+
+			////// Case 15
+			//processor.PreProcessingStages.Add(SplitChannelsStage.BuildValueExtractor());
+			//processor.PreProcessingStages.Add(new BlurStage { Param1 = 9, SmoothType = SmoothType.Gaussian });
+			//processor.PreProcessingStages.Add(PosterizationStage.BuildDefaultLUT(8));
+			//processor.PreProcessingStages.Add(new CannyStage());
+			//processor.PostProcessingStages.Add(new CloneStage());
+
+			////// Case 16
+			//processor.PreProcessingStages.Add(SplitChannelsStage.BuildValueExtractor());
+			//processor.PreProcessingStages.Add(new BlurStage { Param1 = 9, SmoothType = SmoothType.Gaussian });
+			//processor.PreProcessingStages.Add(PosterizationStage.BuildDefaultLUT(32));
+			//processor.PreProcessingStages.Add(new CannyStage());
+			//processor.PostProcessingStages.Add(new CloneStage());
+
+			////// Case 17
+			//processor.PreProcessingStages.Add(SplitChannelsStage.BuildValueExtractor());
+			//processor.PreProcessingStages.Add(new BlurStage { Param1 = 9, SmoothType = SmoothType.Gaussian });
+			//processor.PreProcessingStages.Add(new CannyStage());
+			//processor.PostProcessingStages.Add(new CloneStage());
+
+			////// Case 18
+			//processor.PreProcessingStages.Add(SplitChannelsStage.BuildValueExtractor());
+			//processor.PreProcessingStages.Add(new CannyStage());
+			//processor.PostProcessingStages.Add(new CloneStage());
+
+			////// Case 19
+			//processor.PreProcessingStages.Add(SplitChannelsStage.BuildValueExtractor());
+			//processor.PreProcessingStages.Add(BlurStage.BuildBlur3());
+			//processor.PreProcessingStages.Add(new CannyStage());
+			//processor.PostProcessingStages.Add(new CloneStage());
+
+			////// Case 20
+			//processor.PreProcessingStages.Add(SplitChannelsStage.BuildValueExtractor());
+			//processor.PreProcessingStages.Add(BlurStage.BuildBlur3());
+			//processor.PreProcessingStages.Add(new CannyStage());
+			//processor.PostProcessingStages.Add(new CloneStage());
+
+			////// Case 21
+			//processor.PreProcessingStages.Add(SplitChannelsStage.BuildValueExtractor());
+			//processor.PreProcessingStages.Add(PosterizationStage.BuildDefaultLUT(8));
+			//processor.PreProcessingStages.Add(BlurStage.BuildBlur3());
+			//processor.PreProcessingStages.Add(new CannyStage());
+			//processor.PostProcessingStages.Add(new CloneStage());
+
+			////// Case 22
+			//processor.PreProcessingStages.Add(SplitChannelsStage.BuildValueExtractor());
+			//processor.PreProcessingStages.Add(PosterizationStage.BuildDefaultLUT(32));
+			//processor.PreProcessingStages.Add(BlurStage.BuildBlur3());
+			//processor.PreProcessingStages.Add(new CannyStage());
+			//processor.PostProcessingStages.Add(new CloneStage());
+
+			////// Case 23
+			//processor.PreProcessingStages.Add(SplitChannelsStage.BuildValueExtractor());
+			//processor.PreProcessingStages.Add(new GrayContrastStage { LowerPercentage = 0.15, UpperPercentage = 0.25 });
+			//processor.PreProcessingStages.Add(PosterizationStage.BuildDefaultLUT(32));
+			//processor.PreProcessingStages.Add(BlurStage.BuildBlur3());
+			//processor.PreProcessingStages.Add(new CannyStage());
+			//processor.PostProcessingStages.Add(new CloneStage());
+
+
+			////// Case 24
+			//processor.PreProcessingStages.Add(SplitChannelsStage.BuildValueExtractor());
+			//processor.PreProcessingStages.Add(new GrayContrastStage { LowerPercentage = 0.15, UpperPercentage = 0.25 });
+			//processor.PreProcessingStages.Add(BlurStage.BuildBlur3());
+			//processor.PreProcessingStages.Add(new CannyStage());
+			//processor.PostProcessingStages.Add(new CloneStage());
+
+			////// Case 25
+			//processor.PreProcessingStages.Add(SplitChannelsStage.BuildValueExtractor());
+			//processor.PreProcessingStages.Add(new GrayContrastStage { LowerPercentage = 0.15, UpperPercentage = 0.25 });
+			//processor.PreProcessingStages.Add(BlurStage.BuildBlur3());
+			//processor.PreProcessingStages.Add(new CannyStage());
+			//processor.PostProcessingStages.Add(new CloneStage());
+			//processor.PostProcessingStages.Add(new DetectionStage());
+
+			////// Case 26
+			//processor.PreProcessingStages.Add(SplitChannelsStage.BuildValueExtractor());
+			//processor.PreProcessingStages.Add(new GrayContrastStage { LowerPercentage = 0.15, UpperPercentage = 0.25 });
+			//processor.PreProcessingStages.Add(new BlurStage { Param1 = 9, SmoothType = SmoothType.Gaussian });
+			//processor.PreProcessingStages.Add(new CannyStage());
+			//processor.PostProcessingStages.Add(new CloneStage());
+			//processor.PostProcessingStages.Add(new DetectionStage());
+
+			////// Case 27
+			//processor.PreProcessingStages.Add(SplitChannelsStage.BuildValueExtractor());
+			//processor.PreProcessingStages.Add(new GrayContrastStage { LowerPercentage = 0.15, UpperPercentage = 0.25 });
+			//processor.PreProcessingStages.Add(new BlurStage { Param1 = 9, SmoothType = SmoothType.Gaussian });
+			//processor.PreProcessingStages.Add(PosterizationStage.BuildDefaultLUT(32));
+			//processor.PreProcessingStages.Add(new CannyStage());
+			//processor.PostProcessingStages.Add(new CloneStage());
+			//processor.PostProcessingStages.Add(new DetectionStage());
+
+			////// Case 28
+			//processor.PreProcessingStages.Add(SplitChannelsStage.BuildValueExtractor());
+			//processor.PreProcessingStages.Add(new GrayContrastStage { LowerPercentage = 0.15, UpperPercentage = 0.25 });
+			//processor.PreProcessingStages.Add(new BlurStage { Param1 = 9, SmoothType = SmoothType.Gaussian });
+			//processor.PreProcessingStages.Add(PosterizationStage.BuildDefaultLUT(8));
+			//processor.PreProcessingStages.Add(new CannyStage());
+			//processor.PostProcessingStages.Add(new CloneStage());
+			//processor.PostProcessingStages.Add(new DetectionStage());
+
+			//// Case 29
+			processor.PreProcessingStages.Add(SplitChannelsStage.BuildValueExtractor());
+			processor.PreProcessingStages.Add(new BlurStage { Param1 = 9, SmoothType = SmoothType.Gaussian });
+			processor.PreProcessingStages.Add(PosterizationStage.BuildDefaultLUT(8));
+			processor.PreProcessingStages.Add(new CannyStage());
+			processor.PostProcessingStages.Add(new CloneStage());
+			processor.PostProcessingStages.Add(new DetectionStage());
+
+			////processor.PreProcessingStages.Add(new BGRContrastStage{ LowerPercentage = 0.15, UpperPercentage = 0.25});
+			//processor.PreProcessingStages.Add(SplitChannelsStage.BuildValueExtractor());
+			//processor.PreProcessingStages.Add(new HistogramContrastStage());
+			////processor.PreProcessingStages.Add(new BlurStage { Param1 = 9, SmoothType = SmoothType.Gaussian });
+			////processor.PreProcessingStages.Add(SplitChannelsStage.BuildSaturationExtractor());
+			////processor.PreProcessingStages.Add(new GrayContrastStage { LowerPercentage = 0.15, UpperPercentage = 0.25 });
+			////processor.PreProcessingStages.Add(PosterizationStage.BuildDefaultLUT(32));
+			////processor.PreProcessingStages.Add(BlurStage.BuildBlur3());
+			////processor.PreProcessingStages.Add(new BlurStage { Param1 = 9, SmoothType = SmoothType.Gaussian });
 
             
             
-            //processor.PreProcessingStages.Add(SplitChannelsStage.BuildValueExtractor());
-            //processor.PreProcessingStages.Add(new HistogramContrastStage());
+			////processor.PreProcessingStages.Add(SplitChannelsStage.BuildValueExtractor());
+			////processor.PreProcessingStages.Add(new HistogramContrastStage());
             
-            //processor.PreProcessingStages.Add(new GrayContrastStage
-            //    {
-            //        MaxValue = 255
-            //    });
+			////processor.PreProcessingStages.Add(new GrayContrastStage
+			////    {
+			////        MaxValue = 255
+			////    });
             
-            processor.PreProcessingStages.Add(new CannyStage());
-            //processor.PreProcessingStages.Add(MaskStage.BuildDefaultMask());
+			//processor.PreProcessingStages.Add(new CannyStage());
+			////processor.PreProcessingStages.Add(MaskStage.BuildDefaultMask());
 
-            processor.PostProcessingStages.Add(new CloneStage());
-            processor.PostProcessingStages.Add(new DetectionStage());
+			//processor.PostProcessingStages.Add(new CloneStage());
+			//processor.PostProcessingStages.Add(new DetectionStage());
 
             return processor;
         }
